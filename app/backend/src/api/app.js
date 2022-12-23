6be7cdbc9c46f1cbe5aac0b132815a0cc85cfebb
matheusnff85 <math.marinho154@gmail.com>
2022-12-22 22:52:00 -0300

    feat: adiciona lint e base da api

---
+++ app/backend/src/api/app.js
@@ -0,0 +1,11 @@
+require('express-async-errors');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+module.exports = app;
